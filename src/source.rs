30eb54b939db0108e5a12f4a3f01cceb931f9348
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2024-11-27 11:36:00 +0700

    Implement latest git tag fetching

--- src/source.rs
+++ src/source.rs
@@ -246,3 +246,59 @@ pub fn get_artifact_hash_from_url(url: &Url, unpack: bool) -> Result<String, Get
 
     Ok(response.hash)
 }
+
+#[derive(Debug, Error)]
+pub enum FetchLatestGitTagError {
+    #[error("failed to execute command: {full_command}")]
+    CommandFailed {
+        full_command: String,
+        io_error: io::Error,
+    },
+    #[error("command output is not valid utf8")]
+    CommandOutputInvalidUtf8(#[from] std::string::FromUtf8Error),
+    #[error("no tag fits the provided filter")]
+    NoTagsFitFilter,
+}
+
+pub fn fetch_latest_git_tag(
+    url: &Url,
+    filter: Option<String>,
+) -> Result<String, FetchLatestGitTagError> {
+    let args = [
+        "-c",
+        "versionsort.suffix=-",
+        "ls-remote",
+        "--tags",
+        "--sort=v:refname",
+        url.as_ref(),
+    ];
+
+    let output = Command::new("git").args(args).output().map_err(|e| {
+        FetchLatestGitTagError::CommandFailed {
+            full_command: format!("git {}", args.join(" ")),
+            io_error: e,
+        }
+    })?;
+
+    let output_string = String::from_utf8(output.stdout)?;
+
+    let filter = filter.as_deref().unwrap_or("");
+    let latest_tag = output_string
+        .lines()
+        .map(|line| line.split('/').last().unwrap_or(""))
+        .filter(|line| {
+            line.starts_with(filter)
+                && line
+                    .chars()
+                    .nth(filter.len())
+                    .is_some_and(|c| c.is_ascii_digit())
+        })
+        .last();
+
+    let latest_tag = match latest_tag {
+        Some(tag) => tag,
+        None => return Err(FetchLatestGitTagError::NoTagsFitFilter),
+    };
+
+    Ok(latest_tag.to_string())
+}
